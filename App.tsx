ed09c09157581aa25a1bfed8c07f7bb4bb93d0a1
MACOS <macos@192.168.1.31>
2024-11-25 00:30:57 +0300

    App updated.

--- App.tsx
+++ App.tsx
@@ -1,15 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView } from 'react-native';
+import { SafeAreaView, View } from 'react-native';
 import HomeScreens from './src/screens/HomeScreens';
+import BottomNavigator from './src/navigators/BottomNavigator';
+import { NavigationContainer } from '@react-navigation/native';
 
 
 
 export default function App() {
   return (
-    <SafeAreaView>
-      <StatusBar style="dark" />
-      <HomeScreens />
-    </SafeAreaView>
+    <NavigationContainer >
+      <View style={{ marginTop: 50 }}></View>
+      <StatusBar style="auto" />
+      <BottomNavigator />
+    </NavigationContainer>
   );
 }
 
