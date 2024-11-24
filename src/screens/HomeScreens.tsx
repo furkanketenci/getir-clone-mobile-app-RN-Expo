be973de6cce59ef99ecc065561ff60d94dca295a
MACOS <macos@192.168.1.31>
2024-11-24 17:33:28 +0300

    CategoryList used.

--- src/screens/HomeScreens.tsx
+++ src/screens/HomeScreens.tsx
@@ -2,12 +2,14 @@ import { StyleSheet, ScrollView } from 'react-native'
 import React from 'react'
 import HeaderMain from '../components/home/HeaderMain'
 import SliderCarousel from '../components/home/SliderCarousel'
+import CategoryList from '../components/home/CategoryList'
 
 export default function HomeScreens() {
     return (
         <ScrollView style={styles.homeScreensContainer}>
             <HeaderMain />
             <SliderCarousel />
+            <CategoryList />
         </ScrollView>
     )
 }
