9c476da3a0e5919bab896f9041c18ebbb817556a
MACOS <macos@192.168.1.31>
2024-11-24 17:30:59 +0300

    The unused component was deleted.

--- src/components/home/SliderCarousel.tsx
+++ src/components/home/SliderCarousel.tsx
@@ -1,4 +1,4 @@
-import { View, Text, FlatList, Image, Dimensions, StyleSheet } from 'react-native'
+import { View, FlatList, Image, Dimensions, StyleSheet } from 'react-native'
 import React from 'react'
 
 const sliderImagesDataPaths = [
