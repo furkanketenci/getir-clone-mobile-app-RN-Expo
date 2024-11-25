819053a12b26810df3c9572f2765d7f66145da61
MACOS <furkanktnci@gmail.com>
2024-11-26 00:06:54 +0300

    unnecessary components removed.

--- App.tsx
+++ App.tsx
@@ -1,6 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, View } from 'react-native';
-import HomeScreens from './src/screens/HomeScreens';
+import { View } from 'react-native';
 import BottomNavigator from './src/navigators/BottomNavigator';
 import { NavigationContainer } from '@react-navigation/native';
 
