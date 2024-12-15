c481245296eaa833e5d44ea570f98b55fe20b80b
MACOS <furkanktnci@gmail.com>
2024-12-15 22:59:01 +0300

    MainStackNavigator imported and used.

--- App.tsx
+++ App.tsx
@@ -1,13 +1,14 @@
 import { NavigationContainer } from '@react-navigation/native';
 import BottomTabsNavigator from './src/navigator/bottomNavigatorArea/BottomTabsNavigator';
 import { SafeAreaView, StyleSheet } from 'react-native';
+import MainStackNavigator from './src/navigator/stackNavigatorArea/MainStackNavigator';
 
 export default function App() {
 
   return (
     <SafeAreaView style={styles.container}>
       <NavigationContainer>
-        <BottomTabsNavigator />
+        <MainStackNavigator />
       </NavigationContainer>
     </SafeAreaView>
   );
