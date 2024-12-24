fb40144fdc04fe4c6af544095f66e6ca4aa9b717
MACOS <furkanktnci@gmail.com>
2024-12-25 00:42:00 +0300

    props added and used

--- screens/category/CategoryDetailScreen.tsx
+++ screens/category/CategoryDetailScreen.tsx
@@ -2,12 +2,14 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import { useRoute } from '@react-navigation/native';
 import { ICategoryData } from '../../src/components/homepage/category/HomepageCategoryBox';
+import CategoryFilterbox from '../../src/components/homepage/category/CategoryFilterbox';
 
 export default function CategoryDetailScreen() {
     const route = useRoute();
     const params = route.params as { categoryData: ICategoryData }
     return (
         <View>
+            <CategoryFilterbox paramsInfo={params} />
             <Text>{params.categoryData.title}</Text>
         </View>
     )
