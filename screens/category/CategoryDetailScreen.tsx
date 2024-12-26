506fef656ef7dfe1db84861aba4c3b43f2c792f0
MACOS <furkanktnci@gmail.com>
2024-12-27 02:05:05 +0300

    file path and component name updated.

--- screens/category/CategoryDetailScreen.tsx
+++ screens/category/CategoryDetailScreen.tsx
@@ -1,15 +1,19 @@
-import { View, Text } from 'react-native'
+import { Text, View } from 'react-native'
 import React from 'react'
 import { useRoute } from '@react-navigation/native';
-import { ICategoryData } from '../../src/components/homepage/category/HomepageCategoryBox';
-import CategoryFilterbox from '../../src/components/homepage/category/CategoryFilterbox';
+import { ICategoryData } from '../../src/components/homepage/category/categoryHomepage/HomepageCategoryBox';
+import CategoryTopFilterBox from '../../src/components/homepage/category/categoryTop/CategoryTopFilterBox';
+import CategoryFilterSubBox from '../../src/components/homepage/category/categorySub/CategoryFilterSubBox';
+import { categoriesData } from '../../data/categoryData';
 
 export default function CategoryDetailScreen() {
     const route = useRoute();
     const params = route.params as { categoryData: ICategoryData }
+
     return (
         <View>
-            <CategoryFilterbox paramsInfo={params} />
+            <CategoryTopFilterBox paramsInfo={params.categoryData} />
+            <CategoryFilterSubBox paramsInfo={params.categoryData} />
             <Text>{params.categoryData.title}</Text>
         </View>
     )
