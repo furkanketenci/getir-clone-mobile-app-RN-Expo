0640d4ddb6966b5b08ac9ceb7351144c2af16ef5
MACOS <furkanktnci@gmail.com>
2024-12-27 02:04:34 +0300

    file path changed

--- screens/bottomTabsScreens/HomepageScreen.tsx
+++ screens/bottomTabsScreens/HomepageScreen.tsx
@@ -2,7 +2,7 @@ import { ScrollView, StyleSheet } from 'react-native'
 import React from 'react'
 import HomepageHeader from '../../src/components/homepage/HomepageHeader'
 import HomepageSlider from '../../src/components/homepage/HomepageSlider'
-import HomepageCategoryArea from '../../src/components/homepage/category/HomepageCategoryArea'
+import HomepageCategoryArea from '../../src/components/homepage/category/categoryHomepage/HomepageCategoryArea'
 
 export default function HomepageScreen() {
     return (
