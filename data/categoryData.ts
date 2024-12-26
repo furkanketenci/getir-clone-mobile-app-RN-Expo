32321cc9e1d87eb50271ae6dbbcc2f03f1cdb365
MACOS <furkanktnci@gmail.com>
2024-12-27 02:04:09 +0300

    categoriesData updated.

--- data/categoryData.ts
+++ data/categoryData.ts
@@ -3,91 +3,133 @@ export const categoriesData = [
         imageUrl: require("../assets/categories/1.png"),
         title: "İndirim",
         id: 1,
+        topId: 0,
     },
     {
         imageUrl: require("../assets/categories/2.png"),
         title: "Su & İçecek",
-        id: 2
+        id: 2,
+        topId: 0
     },
     {
         imageUrl: require("../assets/categories/3.png"),
         title: "Meyve & Sebze",
-        id: 3
+        id: 3,
+        topId: 0
     },
     {
         imageUrl: require("../assets/categories/4.png"),
         title: "Fırından",
-        id: 4
+        id: 4,
+        topId: 0
     },
     {
         imageUrl: require("../assets/categories/5.png"),
         title: "Temel Gıda",
-        id: 5
+        id: 5,
+        topId: 0
     },
     {
         imageUrl: require("../assets/categories/6.png"),
         title: "Atıştırmalık",
-        id: 6
+        id: 6,
+        topId: 0
     },
     {
         imageUrl: require("../assets/categories/7.png"),
         title: "Dondurma",
-        id: 7
+        id: 7,
+        topId: 0
     },
     {
         imageUrl: require("../assets/categories/8.png"),
         title: "Süt Ürünleri",
-        id: 8
+        id: 8,
+        topId: 0
     },
     {
         imageUrl: require("../assets/categories/9.png"),
         title: "Kahvaltılık",
-        id: 9
+        id: 9,
+        topId: 0
     },
     {
         imageUrl: require("../assets/categories/10.png"),
         title: "Yiyecek",
-        id: 10
+        id: 10,
+        topId: 0
     },
     {
         imageUrl: require("../assets/categories/11.png"),
         title: "Fit & Form",
-        id: 11
+        id: 11,
+        topId: 0
     },
     {
         imageUrl: require("../assets/categories/12.png"),
         title: "Kişisel Bakım",
-        id: 12
+        id: 12,
+        topId: 0
     },
     {
         imageUrl: require("../assets/categories/13.png"),
         title: "Evcil Hayvan",
-        id: 13
+        id: 13,
+        topId: 0
     },
     {
         imageUrl: require("../assets/categories/14.png"),
         title: "Bebek",
-        id: 14
+        id: 14,
+        topId: 0
     },
     {
         imageUrl: require("../assets/categories/15.png"),
         title: "Cinsel Sağlık",
-        id: 15
+        id: 15,
+        topId: 0
     },
     {
         imageUrl: require("../assets/categories/16.png"),
         title: "Ev Bakım",
-        id: 16
+        id: 16,
+        topId: 0
     },
     {
         imageUrl: require("../assets/categories/17.png"),
         title: "Teknoloji",
-        id: 17
+        id: 17,
+        topId: 0
     },
     {
         imageUrl: require("../assets/categories/18.png"),
         title: "Ev & Yaşam",
-        id: 18
+        id: 18,
+        topId: 0
     },
-
+    {
+        title: "Yılbaşı Ürünleri",
+        id: 20,
+        topId: 1
+    },
+    {
+        title: "Maç Paketi",
+        id: 21,
+        topId: 1
+    },
+    {
+        title: "Sevgililer Günü",
+        id: 22,
+        topId: 1
+    },
+    {
+        title: "Gazlı İçecekler",
+        id: 23,
+        topId: 2
+    },
+    {
+        title: "Gazsız İçecekler",
+        id: 24,
+        topId: 2
+    },
 ]
