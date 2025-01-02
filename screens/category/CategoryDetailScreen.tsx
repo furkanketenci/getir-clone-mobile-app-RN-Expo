c3158cef681a2752b80f82c7c3ca095afa3dfc72
MACOS <furkanktnci@gmail.com>
2025-01-03 01:56:17 +0300

    operations were performed to access the selected category information.

--- screens/category/CategoryDetailScreen.tsx
+++ screens/category/CategoryDetailScreen.tsx
@@ -1,5 +1,5 @@
 import { Text, View } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import { useRoute } from '@react-navigation/native';
 import { ICategoryData } from '../../src/components/homepage/category/categoryHomepage/HomepageCategoryBox';
 import CategoryTopFilterBox from '../../src/components/homepage/category/categoryTop/CategoryTopFilterBox';
@@ -9,12 +9,20 @@ import { categoriesData } from '../../data/categoryData';
 export default function CategoryDetailScreen() {
     const route = useRoute();
     const params = route.params as { categoryData: ICategoryData }
+    const selectedIdInfo = params.categoryData
 
+    const [selectedTopFilterCategory, setSelectedTopFilterCategory] = useState<ICategoryData>(selectedIdInfo);
+
+    const handleSelectedTopFilterCtg = (selectedId: ICategoryData) => {
+        const selectedCategory = categoriesData.find((item) => item.id === selectedId.id);
+        if (selectedCategory) {
+            setSelectedTopFilterCategory(selectedCategory);
+        }
+    }
     return (
         <View>
-            <CategoryTopFilterBox paramsInfo={params.categoryData} />
+            <CategoryTopFilterBox selectedTopFilterFunc={handleSelectedTopFilterCtg} aboutSelectedFilterData={selectedTopFilterCategory} />
             <CategoryFilterSubBox paramsInfo={params.categoryData} />
-            <Text>{params.categoryData.title}</Text>
         </View>
     )
 }
